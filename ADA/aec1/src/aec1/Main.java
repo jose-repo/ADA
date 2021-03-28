a303a98aa2815d7203a36484c979161b66672fcb
jose <jmgvizuete@gmail.com>
2021-03-28 11:41:18 +0200

    comentarios y correcciones

--- ADA/aec1/src/aec1/Main.java
+++ ADA/aec1/src/aec1/Main.java
@@ -13,12 +13,14 @@ public class Main {
 
     public static void a1() {
         long start = System.nanoTime();
-        String result = "0";
-        for (int i = 2; i <=1000000; i++) {
+        long result = 0;
+        for (int i = 2; i <= 1000000; i++) {
             for (int e = 1; e <= 100; e++) {
                 result = mA(i);
             }
-            //System.out.println(i + " x " + (i - 1) + " = " + new BigInteger(result));
+            if (i == 55555) {
+                System.out.println(i + " x " + (i - 1) + " = " + result);
+            }
         }
         long elapsedTime = System.nanoTime() - start;
         System.out.println("A1 seconds : " + formatTime(elapsedTime));
@@ -26,12 +28,14 @@ public class Main {
 
     public static void a2() {
         long start = System.nanoTime();
-        int result = 0;
+        long result = 0;
         for (int i = 2; i <= 1000000; i++) {
             for (int e = 1; e <= 100; e++) {
                 result = mDV(i);
             }
-            // System.out.println(i + " x " + (i - 1) + " = " + result);
+            if (i == 55555) {
+                System.out.println(i + " x " + (i - 1) + " = " + result);
+            }
         }
         long elapsedTime = System.nanoTime() - start;
         System.out.println("A2 seconds: " + formatTime(elapsedTime));
@@ -43,43 +47,27 @@ public class Main {
      * @param n
      * @return
      */
-    private static String mA(int n) {
-        String strDigitsN1 = String.valueOf(n - 1);
-        int totalCols = strDigitsN1.length() * 2;
-        int rowIndex = 0;
-        int [][] calc = new int[strDigitsN1.length()][strDigitsN1.length() * 2];
-        for (int i = strDigitsN1.length() - 1; i >= 0; i--) {
-           int digit =  Character.getNumericValue(strDigitsN1.charAt(i));
-           String subResult = String.valueOf(n * digit);
-           int indexSub = subResult.length() -1;
-            for (int j = totalCols - 1; j >= 0; j--) {
-                if (indexSub >= 0) {
-                    calc[rowIndex][j] = Character.getNumericValue(subResult.charAt(indexSub));
-                    indexSub--;
-                }
+    private static long mA(int n) {
+        String strMultiplicand = String.valueOf(n - 1);
+        long result = 0;
+        long currentValue = 0;
+        long pow = 0;
+        int index = 0;
+        // se recorren todos los dígitos del multiplicador
+        for (int i = strMultiplicand.length() - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(strMultiplicand.charAt(i));
+            currentValue = (n * digit);
+            if (index > 0) {
+                // se colocan los ceros a la derecha
+                // por cada posición del multiplicador, se añade un cero
+                pow = currentValue * Integer.valueOf((int) Math.pow(10, index));
             }
-            rowIndex ++;
-            totalCols --;
-        }
-        String result = "";
-        String restStr = "0";
-        for (int j = calc[0].length-1; j >= 0; j--) {
-            int colResult = 0;
-            int rest = Integer.parseInt(restStr);
-            // sumar columna
-            for (int i = 0; i < strDigitsN1.length(); i++) {
-                colResult = calc[i][j] + colResult + rest;
-                rest = 0;
+            if (pow == 0) {
+                pow = currentValue;
             }
-            // sacar el resto de la suma
-            // ejemplo de una suma de 12
-            // se coge el 2 y el resto 1 se suma en la siguiente
-            char[] array = String.valueOf(colResult).toCharArray();
-            result = array[array.length - 1] + result;
-            restStr = "0";
-            if (array.length == 2) { // resto
-                restStr += array[0];
-            }
+            // se suma al resultado obtenido hasta el momento
+            result += pow;
+            index++;
         }
         return result;
     }
@@ -90,15 +78,18 @@ public class Main {
      * @param n
      * @return
      */
-    private static int mDV(int n) {
+    private static long mDV(int n) {
         if (n == 0) {
             return 0;
         }
+        // para dividir los valores se utilizan
+        // string y array de string
         String strDigitsN = String.valueOf(n);
         String strDigitsN1 = String.valueOf(n - 1);
 
         String[] arrayN = new String[strDigitsN.length()];
         String[] arrayN1 = new String[strDigitsN1.length()];
+        // se pasa el string al array
         for (int i = 0; i < strDigitsN.length(); i++) {
             arrayN[i] = String.valueOf(strDigitsN.charAt(i));
         }
@@ -111,60 +102,78 @@ public class Main {
         int b = 0;
         int c = 0;
         int d = 0;
-        // set values to variables "a" and "b"
-        if (arrayN.length == 1) {
-            a = 0;
-            b = Integer.valueOf(arrayN[0]);
-        } else if (arrayN.length == 2) {
-            a = 0;
-            b = Integer.parseInt(arrayN[0] + arrayN[1]);
-        } else if (arrayN.length == 3) {
-            a = Integer.valueOf(arrayN[0]);
-            b = Integer.parseInt(arrayN[1] + arrayN[2]);
-        } else if (arrayN.length == 4) {
-            a = Integer.parseInt(arrayN[0] + arrayN[1]);
-            b = Integer.parseInt(arrayN[2] + arrayN[3]);
-        } else if (arrayN.length == 5) {
-            a = Integer.parseInt(arrayN[0] + arrayN[1]);
-            b = Integer.parseInt(arrayN[2] + arrayN[3]+ arrayN[4]);
-        } else if (arrayN.length == 6) {
-            a = Integer.parseInt(arrayN[0] + arrayN[1] + arrayN[2]);
-            b = Integer.parseInt(arrayN[3] + arrayN[4] + arrayN[5]);
-        } else if (arrayN.length == 7) {
-            a = Integer.parseInt(arrayN[0] + arrayN[1] + arrayN[2]);
-            b = Integer.parseInt(arrayN[3] + arrayN[4] + arrayN[5] + arrayN[6]);
+        // INICIALIZAR BLOQUE A y B -----------------------------
+        String concatenateZero = "";
+        // si el número es impar se rellena con 0 por la izquierda
+        if (arrayN.length %2 != 0) {
+            concatenateZero = "0";
         }
-        // set values to variables "c" and "d"
-        if (arrayN1.length == 1) {
-            c = 0;
-            d = Integer.valueOf(arrayN1[0]);
-        } else if (arrayN1.length == 2) {
-            c = 0;
-            d = Integer.parseInt(arrayN1[0] + arrayN1[1]);
-        } else if (arrayN1.length == 3) {
-            c = Integer.valueOf(arrayN1[0]);
-            d = Integer.parseInt(arrayN1[1] + arrayN1[2]);
-        } else if (arrayN1.length == 4) {
-            c = Integer.parseInt(arrayN1[0] + arrayN1[1]);
-            d = Integer.parseInt(arrayN1[2] + arrayN1[3]);
-        } else if (arrayN1.length == 5) {
-            c = Integer.parseInt(arrayN1[0] + arrayN1[1]);
-            d = Integer.parseInt(arrayN1[2] + arrayN1[3] + arrayN1[4]);
-        } else if (arrayN1.length == 6) {
-            c = Integer.parseInt(arrayN1[0] + arrayN1[1] + arrayN1[2]);
-            d = Integer.parseInt(arrayN1[3] + arrayN1[4] + arrayN1[5]);
-        } else if (arrayN1.length == 7) {
-            c = Integer.parseInt(arrayN1[0] + arrayN1[1] + arrayN1[2]);
-            d = Integer.parseInt(arrayN1[3] + arrayN1[4] + arrayN1[5] + arrayN1[6]);
+        int half = arrayN.length / 2;
+        String aStr = concatenateZero;
+        String bStr = "";
+        // primera mitad del número
+        // ejemplo para 1122 coge el 11
+        for (int i = 0; i < half; i++) {
+            aStr += arrayN[i];
         }
-        // calculate axc axd bxc bxd
+        // segunda mitad del número
+        // ejemplo para 1122 coge el 22
+        for (int i = half; i < arrayN.length; i++) {
+            bStr += arrayN[i];
+        }
+        if (bStr.isEmpty()) {
+            bStr = "0";
+        }
+        if (aStr.isEmpty()) {
+            aStr = "0";
+        }
+        // inicializa la variable a y b
+        a = Integer.valueOf(aStr);
+        b = Integer.valueOf(bStr);
+
+        // INICIALIZAR BLOQUE B y C -----------------------------
+        concatenateZero = "";
+        // si el número es impar se rellena con 0 por la izquierda
+        if (arrayN1.length %2 != 0) {
+            concatenateZero = "0";
+        }
+        half = arrayN1.length / 2;
+        String cStr = concatenateZero;
+        String dStr = "";
+        for (int i = 0; i < half; i++) {
+            cStr += arrayN1[i];
+        }
+        for (int i = half; i < arrayN1.length; i++) {
+            dStr += arrayN1[i];
+        }
+        if (cStr.isEmpty()) {
+            cStr = "0";
+        }
+        if (dStr.isEmpty()) {
+            dStr = "0";
+        }
+        // inicializar c y d
+        c = Integer.valueOf(cStr);
+        d = Integer.valueOf(dStr);
+        // calcular axc axd bxc bxd
         int ac = a * c;
         int ad = a * d;
         int bc = b * c;
         int bd = b * d;
         // final result
         // (axc)x10⁴+(axd+bxc)x10²+(bxd)
-        return (int) ((int) (ac * Math.pow(10, 4)) + (ad + bc) * Math.pow(10, 2) + (bd));
+        int exp = strDigitsN1.length();
+        int exp2 = exp / 2;
+        // si el número es impar se suma uno a cada exponente
+        if (exp %2 != 0) {
+            exp += 1;
+            exp2 += 1;
+        }
+
+        long acR = (long) (ac * Math.pow(10, exp));
+        long adR = (long) ((ad) * Math.pow(10, exp2));
+        long bcR = (long) ((bc) * Math.pow(10, exp2));
+        return (acR + adR + bcR + (bd));
     }
 
     /**
