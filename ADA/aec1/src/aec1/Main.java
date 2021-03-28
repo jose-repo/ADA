976957a5d803d9e236c6b55781ccb0790363c2bb
jose <jmgvizuete@gmail.com>
2021-03-28 01:46:19 +0100

    terminado

--- ADA/aec1/src/aec1/Main.java
+++ ADA/aec1/src/aec1/Main.java
@@ -1,5 +1,7 @@
 package aec1;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,24 +10,28 @@ public class Main {
         a2();
         System.out.println("end");
     }
-    // multiplicación a la russe
+
     public static void a1() {
         long start = System.nanoTime();
-        for (int i = 2; i <= 1000000; i++) {
+        String result = "0";
+        for (int i = 2; i <=1000000; i++) {
             for (int e = 1; e <= 100; e++) {
-                int result = mA(i);
+                result = mA(i);
             }
+            //System.out.println(i + " x " + (i - 1) + " = " + new BigInteger(result));
         }
         long elapsedTime = System.nanoTime() - start;
         System.out.println("A1 seconds : " + formatTime(elapsedTime));
     }
-    // divide y vencerás
+
     public static void a2() {
         long start = System.nanoTime();
-        for (int i = 2; i <= 100000; i++) {
+        int result = 0;
+        for (int i = 2; i <= 1000000; i++) {
             for (int e = 1; e <= 100; e++) {
-                int result = mDV(i);
+                result = mDV(i);
             }
+            // System.out.println(i + " x " + (i - 1) + " = " + result);
         }
         long elapsedTime = System.nanoTime() - start;
         System.out.println("A2 seconds: " + formatTime(elapsedTime));
@@ -37,8 +43,45 @@ public class Main {
      * @param n
      * @return
      */
-    private static int mA(int n) {
-        return n * (n - 1);
+    private static String mA(int n) {
+        String strDigitsN1 = String.valueOf(n - 1);
+        int totalCols = strDigitsN1.length() * 2;
+        int rowIndex = 0;
+        int [][] calc = new int[strDigitsN1.length()][strDigitsN1.length() * 2];
+        for (int i = strDigitsN1.length() - 1; i >= 0; i--) {
+           int digit =  Character.getNumericValue(strDigitsN1.charAt(i));
+           String subResult = String.valueOf(n * digit);
+           int indexSub = subResult.length() -1;
+            for (int j = totalCols - 1; j >= 0; j--) {
+                if (indexSub >= 0) {
+                    calc[rowIndex][j] = Character.getNumericValue(subResult.charAt(indexSub));
+                    indexSub--;
+                }
+            }
+            rowIndex ++;
+            totalCols --;
+        }
+        String result = "";
+        String restStr = "0";
+        for (int j = calc[0].length-1; j >= 0; j--) {
+            int colResult = 0;
+            int rest = Integer.parseInt(restStr);
+            // sumar columna
+            for (int i = 0; i < strDigitsN1.length(); i++) {
+                colResult = calc[i][j] + colResult + rest;
+                rest = 0;
+            }
+            // sacar el resto de la suma
+            // ejemplo de una suma de 12
+            // se coge el 2 y el resto 1 se suma en la siguiente
+            char[] array = String.valueOf(colResult).toCharArray();
+            result = array[array.length - 1] + result;
+            restStr = "0";
+            if (array.length == 2) { // resto
+                restStr += array[0];
+            }
+        }
+        return result;
     }
 
     /**
@@ -54,10 +97,15 @@ public class Main {
         String strDigitsN = String.valueOf(n);
         String strDigitsN1 = String.valueOf(n - 1);
 
-        // split n to array
-        String[] arrayN = strDigitsN.split("(?<=\\d)(?=\\D)|(?=\\d)(?<=\\D)");
-        // split n-1 to array
-        String[] arrayN1 = strDigitsN1.split("(?<=\\d)(?=\\D)|(?=\\d)(?<=\\D)");
+        String[] arrayN = new String[strDigitsN.length()];
+        String[] arrayN1 = new String[strDigitsN1.length()];
+        for (int i = 0; i < strDigitsN.length(); i++) {
+            arrayN[i] = String.valueOf(strDigitsN.charAt(i));
+        }
+        for (int i = 0; i < strDigitsN1.length(); i++) {
+            arrayN1[i] = String.valueOf(strDigitsN1.charAt(i));
+        }
+
         // init variable
         int a = 0;
         int b = 0;
@@ -97,16 +145,16 @@ public class Main {
             c = Integer.valueOf(arrayN1[0]);
             d = Integer.parseInt(arrayN1[1] + arrayN1[2]);
         } else if (arrayN1.length == 4) {
-            c = Integer.parseInt(arrayN1[0] + arrayN[1]);
+            c = Integer.parseInt(arrayN1[0] + arrayN1[1]);
             d = Integer.parseInt(arrayN1[2] + arrayN1[3]);
         } else if (arrayN1.length == 5) {
-            c = Integer.parseInt(arrayN1[0] + arrayN[1]);
+            c = Integer.parseInt(arrayN1[0] + arrayN1[1]);
             d = Integer.parseInt(arrayN1[2] + arrayN1[3] + arrayN1[4]);
         } else if (arrayN1.length == 6) {
-            c = Integer.parseInt(arrayN1[0] + arrayN[1] + arrayN1[2]);
+            c = Integer.parseInt(arrayN1[0] + arrayN1[1] + arrayN1[2]);
             d = Integer.parseInt(arrayN1[3] + arrayN1[4] + arrayN1[5]);
         } else if (arrayN1.length == 7) {
-            c = Integer.parseInt(arrayN1[0] + arrayN[1] + arrayN1[2]);
+            c = Integer.parseInt(arrayN1[0] + arrayN1[1] + arrayN1[2]);
             d = Integer.parseInt(arrayN1[3] + arrayN1[4] + arrayN1[5] + arrayN1[6]);
         }
         // calculate axc axd bxc bxd
